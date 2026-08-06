15b8b975529a1d6bd87f723e62bb5afb63fbdb39
jatinnathh <jatinnath1111@gmail.com>
2026-08-06 18:38:10 +0530

    landing page default . cahnges needed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+'use client'
 import SceneWrapper from '@/components/SceneWrapper'
 import ScrollUIOverlay from '@/components/ScrollUIOverlay'
-
+import { div } from 'three/src/nodes/math/OperatorNode.js'
+import { useRouter } from 'next/navigation'
 export default function Home() {
+  const router = useRouter()
   return (
-    <main className="relative w-screen h-screen overflow-hidden">
-      <SceneWrapper />
-      <ScrollUIOverlay />
-    </main>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <button style={{ color: "white", backgroundColor: "black", padding: "10px", borderRadius: "10px", cursor: "pointer" }} onClick={() => router.push("/dashboard")}>Go to Dashboard</button>
+    </div>
   )
 }
